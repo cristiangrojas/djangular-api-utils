fe01c25b4b7cf938a5724d52440dcc45db3a1129
Cristian Rojas <cristianguillermorojas@gmail.com>
2015-08-09 10:30:00 -0500

    I missed to include url-join function in the previous release

--- dist/djangular-api-utils.js
+++ dist/djangular-api-utils.js
@@ -1,3 +1,26 @@
+// url-join function
+// taken from https://github.com/jfromaniello/url-join
+(function (name, context, definition) {
+  if (typeof module !== 'undefined' && module.exports) module.exports = definition();
+  else if (typeof define === 'function' && define.amd) define(definition);
+  else context[name] = definition();
+})('urljoin', this, function () {
+
+  function normalize (str) {
+    return str
+            .replace(/[\/]+/g, '/')
+            .replace(/\/\?/g, '?')
+            .replace(/\/\#/g, '#')
+            .replace(/\:\//g, '://');
+  }
+
+  return function () {
+    var joined = [].slice.call(arguments, 0).join('/');
+    return normalize(joined);
+  };
+
+});
+
 (function(){
   'use strict';
 
